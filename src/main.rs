1ab7fa419d77dc23ad5eac891b31db5171ba283f
hendisantika <hendisantika@yahoo.co.id>
2024-05-07 09:17:48 +0700

    Add AppState Struct 🫸🌀✏️📗🐧🐳

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+mod handler;
 mod model;
 mod route;
 mod schema;
-mod handler;
+
+use std::sync::Arc;
+
+use axum::http::{header::CONTENT_TYPE, Method};
+
+use dotenv::dotenv;
+use tokio::net::TcpListener;
+
+use sqlx::mysql::{MySqlPool, MySqlPoolOptions};
+
+use route::create_router;
+use tower_http::cors::{Any, CorsLayer};
+
+pub struct AppState {
+    db: MySqlPool,
+}
 
 fn main() {
     println!("Hello, world!");
