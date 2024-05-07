35292127d3091ffb9d4b1db2089a90786905aeda
hendisantika <hendisantika@yahoo.co.id>
2024-05-07 09:15:36 +0700

    Add get_note_handler 🫸🌀✏️📗🐧🐳

--- src/handler.rs
+++ src/handler.rs
@@ -144,3 +144,50 @@ pub async fn create_note_handler(
 
     Ok(Json(note_response))
 }
+
+pub async fn get_note_handler(
+    Path(id): Path<uuid::Uuid>,
+    State(data): State<Arc<AppState>>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    // // get using query macro
+    // let query_result = sqlx::query_as!(
+    //     NoteModel,
+    //     r#"SELECT * FROM notes WHERE id = ?"#,
+    //     id.to_string()
+    // )
+    // .fetch_one(&data.db)
+    // .await;
+
+    // get not using query macro
+    let query_result = sqlx::query_as::<_, NoteModel>(r#"SELECT * FROM notes WHERE id = ?"#)
+        .bind(id.to_string())
+        .fetch_one(&data.db)
+        .await;
+
+    // check & response
+    match query_result {
+        Ok(note) => {
+            let note_response = serde_json::json!({
+                "status": "success",
+                "data": serde_json::json!({
+                    "note": to_note_response(&note)
+                })
+            });
+
+            return Ok(Json(note_response));
+        }
+        Err(sqlx::Error::RowNotFound) => {
+            let error_response = serde_json::json!({
+                "status": "fail",
+                "message": format!("Note with ID: {} not found", id)
+            });
+            return Err((StatusCode::NOT_FOUND, Json(error_response)));
+        }
+        Err(e) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"status": "error","message": format!("{:?}", e)})),
+            ));
+        }
+    };
+}
