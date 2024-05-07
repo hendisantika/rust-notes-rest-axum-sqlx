f8981ab87cfaf1fdfa566021287d45c24d2319b6
hendisantika <hendisantika@yahoo.co.id>
2024-05-07 09:02:06 +0700

    Add create_router 🫸🌀✏️📗🐧🐳

--- src/route.rs
+++ src/route.rs
@@ -12,3 +12,17 @@ use crate::{
     },
     AppState,
 };
+
+pub fn create_router(app_state: Arc<AppState>) -> Router {
+    Router::new()
+        .route("/api/healthcheck", get(health_check_handler))
+        .route("/api/notes", post(create_note_handler))
+        .route("/api/notes", get(note_list_handler))
+        .route(
+            "/api/notes/:id",
+            get(get_note_handler)
+                .patch(edit_note_handler)
+                .delete(delete_note_handler),
+        )
+        .with_state(app_state)
+}
